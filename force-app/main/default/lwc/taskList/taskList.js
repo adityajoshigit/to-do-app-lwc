4f6f1d70feeb264c4b14f6b6e979fd8d7c57dd9e
adityajoshigit <adityasjoshi96@gmail.com>
2023-01-08 19:05:12 -0500

    basic code for taskList cmp completed

---
+++ force-app/main/default/lwc/taskList/taskList.js
@@ -0,0 +1,35 @@
+import { LightningElement, track, api, wire } from 'lwc';
+
+export default class TaskList extends LightningElement {
+    originalTaskList = [];
+
+    @api listTitle = '';
+
+    @api 
+    get tasks() {
+        this.originalTaskList;
+    }
+    set tasks(value) {
+        this.setAttribute('tasks', value);
+        value.forEach(element => {
+            element.isComplete = (element.Status__c === 'Done');
+        });
+        this.originalTaskList = value;
+        this.tasksToShow = value;
+    }
+    
+    @track tasksToShow = [];
+    @track _tasks = [];
+
+    // handlers
+    onSearchInputKeyUp(event) {
+        const searchText = event.target.value;
+        if (searchText.trim() === '') {
+            this.tasksToShow = this.originalTaskList;
+        } else {
+            if ((this.originalTaskList).length) {
+                this.tasksToShow = this.originalTaskList.filter(t => t.Short_Description__c.includes(searchText));
+            }
+        }
+    }
+}
