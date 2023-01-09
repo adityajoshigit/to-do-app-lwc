68c9d3442afb2753285f4d8f9634579ab5992ce0
adityajoshigit <adityasjoshi96@gmail.com>
2023-01-08 21:25:47 -0500

    basic to-do list creation and search working

--- force-app/main/default/lwc/taskList/taskList.js
+++ force-app/main/default/lwc/taskList/taskList.js
@@ -1,34 +1,36 @@
-import { LightningElement, track, api, wire } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 
 export default class TaskList extends LightningElement {
     originalTaskList = [];
-
-    @api listTitle = '';
+    @api
+    listTitle = '';
+    
     @api 
     get tasks() {
-        this.originalTaskList;
+        return this.originalTaskList;
     }
     set tasks(value) {
-        this.setAttribute('tasks', value);
-        console.log('value = ' + value);
+        value = value || [];
         value.forEach(element => {
-            element.isComplete = (element.Status__c === 'Done');
+            const taskelement = {
+                ...element,
+                isComplete: (element.Status__c === 'Done')
+            }
+            this._tasks.push(taskelement);
+            this.originalTaskList.push(taskelement);
         });
-        this.originalTaskList = value;
-        this.tasksToShow = value;
     }
-    
-    @track tasksToShow = [];
+
     @track _tasks = [];
 
     // handlers
     onSearchInputKeyUp(event) {
         const searchText = event.target.value;
         if (searchText.trim() === '') {
-            this.tasksToShow = this.originalTaskList;
+            this._tasks = this.originalTaskList;
         } else {
             if ((this.originalTaskList).length) {
-                this.tasksToShow = this.originalTaskList.filter(t => t.Short_Description__c.includes(searchText));
+                this._tasks = this.originalTaskList.filter(t => t.Short_Description__c.includes(searchText));
             }
         }
     }
--- force-app/main/default/lwc/toDoApp/toDoApp.js
+++ force-app/main/default/lwc/toDoApp/toDoApp.js
@@ -2,17 +2,26 @@ import { LightningElement, track, wire } from 'lwc';
 import getAllTasks from '@salesforce/apex/ToDoController.getAllTasks';
 
 export default class ToDoApp extends LightningElement {
-    @track allTasks;
     
-    @wire(getAllTasks, {status: null})
-    wiredGetAllTasks({ data, error}) {
-        console.log('data = ' + data);
-        console.log('error = ' + error);
-        if (data) {
-            
-        } else {
-            
+    @track
+    allTasks = [];
+
+    @wire(getAllTasks)
+    wiredGetAllTasks(
+        {
+            data,
+            error
         }
+    ) {
+        if (data) {
+            this.allTasks = data;
+        } else if(error) {
+            this.allTasks = [];
+        }
+    }
+
+    get taskListData() {
+        return this.allTasks.data;
     }
 
 }
